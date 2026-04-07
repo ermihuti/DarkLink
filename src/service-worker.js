61360338408e8d10ae5d3a27a4bbbf3a68a299a6
Glejdi <glenik@yea.al>
2026-04-07 11:07:20 +0200

    Add service worker file for PWA support

---
+++ src/service-worker.js
@@ -0,0 +1,39 @@
+/// <reference types="@sveltejs/kit" />
+import { build, files, version } from '$service-worker';
+
+const CACHE_NAME = `darklink-${version}`;
+const APP_ASSETS = [...build, ...files, '/'];
+
+self.addEventListener('install', (event) => {
+	event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_ASSETS)));
+});
+
+self.addEventListener('activate', (event) => {
+	event.waitUntil(
+		caches
+			.keys()
+			.then((keys) =>
+				Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+			)
+	);
+});
+
+self.addEventListener('fetch', (event) => {
+	if (event.request.method !== 'GET') return;
+
+	event.respondWith(
+		caches.match(event.request).then((cached) => {
+			if (cached) return cached;
+
+			return fetch(event.request)
+				.then((response) => {
+					if (response.ok && event.request.url.startsWith(self.location.origin)) {
+						const clone = response.clone();
+						caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+					}
+					return response;
+				})
+				.catch(() => caches.match('/'));
+		})
+	);
+});
